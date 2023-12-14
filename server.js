fa70641e65f8d46f3340bec31689fd7debc0db02
mhgarry <mhgarry92@gmail.com>
2023-12-14 01:22:49 -0500

    use fs module to get our data

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require('http'); //require http module for networking capabilities
 const url = require('url'); // require url for url parsing and routing
 require('dotenv').config(); // to define our environment variables
+const fs = require('fs'); // to read our data from json file
 
 const PORT = process.env.PORT || 8888;
 const HOST = process.env.HOST;
+
+//fetch data at start of application to have ready
+const batData = fs.readFileSync(`${__dirname}/data/bats.json`);
+// create a data object to work with in our later templates
+const batDataObject = JSON.parse(batData);
+console.log(batDataObject);
 
 // create server instance
 const server = http.createServer((req, res) => {
