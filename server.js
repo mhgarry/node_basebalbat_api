5bc32f0ab81f511db32681d20eb5e2bd04e63564
mhgarry <mhgarry92@gmail.com>
2023-12-14 21:02:35 -0500

    change landing page background+load templates into server file

--- server.js
+++ server.js
@@ -7,10 +7,24 @@ const PORT = process.env.PORT || 8888;
 const HOST = process.env.HOST;
 
 //fetch data at start of application to have ready
-const batData = fs.readFileSync(`${__dirname}/data/bats.json`);
+const batData = fs.readFileSync(`${__dirname}/data/bats.json`, 'utf-8');
 // create a data object to work with in our later templates
 const batDataObject = JSON.parse(batData);
 console.log(batDataObject);
+
+// read and load our templates
+const tempLanding = fs.readFileSync(
+  `${__dirname}/templates/landing-page.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/bat-card.html`,
+  'utf-8'
+);
+const tempBat = fs.readFileSync(
+  `${__dirname}/templates/bat-page.html`,
+  'utf-8'
+);
 
 // create server instance
 const server = http.createServer((req, res, err) => {
